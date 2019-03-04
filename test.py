b56c284f158406e4f6fe68ba6b34d7d0b6b6ceb6
Heather Herbert <Heather.Herbert.1975@gmail.com>
2019-03-04 08:20:10 +0000

    Tidyed up sentiment modual and added breaks into test.py

--- test.py
+++ test.py
@@ -3,16 +3,19 @@ import keywords_mod as k
 
 phrase = "Northgate is necessary to our line of business. Heather solved the problem quickly and effectively, she was a pleasure to work with"
 print(phrase)
-print(s.sentiment(phrase))
+print(s.sentiment(phrase)[0])
 print(k.keyWords(phrase))
+input("Press enter.")
 
 phrase = "Heather didn't solve the problem, due to no knowlege of how the system worked."
 
 print(phrase)
-print(s.sentiment(phrase))
+print(s.sentiment(phrase)[0])
 print(k.keyWords(phrase))
+input("Press enter.")
 
 phrase = "James needs to ensure the customer is happy, and not just himself."
 print(phrase)
-print(s.sentiment(phrase))
+print(s.sentiment(phrase)[0])
 print(k.keyWords(phrase))
+input("Press enter.")
